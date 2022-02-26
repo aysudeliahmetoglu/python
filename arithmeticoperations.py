20716348f03e6d7aa5a7db7fddd9465f47c5c105
aysu <aysudeliahmetoglu@gmail.com>
2022-02-26 19:48:21 +0300

    arithmetic operations

---
+++ arithmeticoperations.py
@@ -0,0 +1,31 @@
+
+#change the value of data types
+a=4
+b=3
+a,b=b,a
+#arithmetic operations
+c=a+b
+k=a*b
+#integer division
+l=345
+m=6
+x=345//6
+#remainder finder operator
+y=345%6
+#exponentiation operator
+y=4**3
+#find square root
+y=25**0.5
+#find cube
+y=8**(1/3)
+#strings
+a="aysu"
+len(a)
+
+b=-13.4
+#sep parameter
+# print(27,4,2001,sep ="/")
+print(*"I am learning Python")
+#formating
+asd="{} {} {} ".format(3.234344,23456578,34536478)
+print(asd)
