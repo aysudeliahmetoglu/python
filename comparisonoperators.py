dfff5b765de1b12f5250c1f07a23f1e0d694b9f1
aysu <aysudeliahmetoglu@gmail.com>
2022-02-28 21:11:16 +0300

    comparison operators

---
+++ comparisonoperators.py
@@ -0,0 +1,10 @@
+# == true if two values ​​are equal otherwise false
+# != true if the two values ​​are not equal false if they are equal
+# > true if the value on the left is greater than the value on the right
+# < true if the value on the right s greater than the value on the left
+# >= true if the left value is greater than or equal to the right
+# <= true if the right value is greater than or equal to the left
+print("mehmet" == "mehmet") #true
+print("mehmet" == "murat") #false
+print(1.2 >2.5) #false
+print("car"<"desk") #true because c comes first in the alphabet
