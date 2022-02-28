4797e8bdc486ebf49b79a862bd410514244a9b4a
aysu <aysudeliahmetoglu@gmail.com>
2022-02-28 03:15:23 +0300

    player registration program

---
+++ playerregistration.py
@@ -0,0 +1,9 @@
+#player registration program
+name=input("player name: ")
+surname=input("player surname: ")
+team=input("player team:  ")
+information=[name,surname,team]
+print("information is being saved")
+print("player name:{}\nplayer surname: {}\nplayer team{}".format(information[0],information[1],information[2]))
+print("information saved")
+
