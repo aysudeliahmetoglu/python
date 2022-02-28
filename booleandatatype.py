68e660d055d4d384b4e30780e7c15a644a5d1969
aysu <aysudeliahmetoglu@gmail.com>
2022-02-28 20:54:55 +0300

    boolean data type

---
+++ booleandatatype.py
@@ -0,0 +1,10 @@
+#this data type has only 2 values. true and false
+a=True  
+print(type(a))   #to print type of value
+b=False
+print(type(b))
+#With the bool method, we return true if the number is different from 0 and false if 0.
+print(bool(12))  #true
+print(bool(0))  #false
+print(1==1)  #true
+a=None #value not yet assigned
