e47eb732a5791d53a01419cb77a24e2a956dced9
aysu <aysudeliahmetoglu@gmail.com>
2022-03-01 21:39:12 +0300

    conditional states

--- conditionalstates.py
+++ conditionalstates.py
@@ -4,16 +4,16 @@ age=int(input("please enter your age"))
 if(age < 18):
     print("you cannot enter this place!")
 else:
-    print("welcome this place!") 
+    print("welcome to this place!") 
 
-number=int(input("please enter number"))
+number=int(input("please enter a number"))
 
 if(number <0):
     print("this number is negative")
 else:
-    print("number can be 0 or positive")    
+    print("this number can be 0 or positive")    
 #if-elif-else
-operation=input("please choose operation:")
+operation=input("please choose an operation:")
 if operation=="1":
     print("operation 1 selected")
 elif operation=="2":
@@ -26,7 +26,7 @@ else:                                   #else block is not used alone
     print("invalid operation")
 
 #grade evaluation system
-note=int(input("please enter note:"))
+note=int(input("please enter a note:"))
 if(note>=90):
     print("AA")
 elif(note >=85):
