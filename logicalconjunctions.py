6eb9a48432bb853967adb17972caa95cb38dd389
aysu <aysudeliahmetoglu@gmail.com>
2022-03-01 21:41:08 +0300

    logical conjunctions

--- logicalconjunctions.py
+++ logicalconjunctions.py
@@ -1,10 +1,10 @@
 #and operator  -all conditions must be true for the general condition to be true-
-1<2 and "murat" == "murat" #true
-1>2 and "murat" == "murat" #false
-1<2 and "car"<"desk" and 23.41>12.5 #true
+print(1<2 and "murat" == "murat") #true
+pprint(1>2 and "murat" == "murat") #false
+print(1<2 and "car"<"desk" and 23.41>12.5) #true
 #or operator   -not all conditions need to be true for the general condition to be true-
-1<2 or "murat"!="murat" #true
-11<2 or "car"<"desk" or 23.41>12.5 #true
+print(1<2 or "murat"!="murat") #true
+print(11<2 or "car"<"desk" or 23.41>12.5) #true
 #not operator
-2==2 #true
-not 2==2 #false
+print(2==2) #true
+print(not 2==2)#false
