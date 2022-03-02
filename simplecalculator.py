4e639b9db58fb2309ba0ef4c5e372fa8f54a817d
aysu <aysudeliahmetoglu@gmail.com>
2022-03-02 23:23:34 +0300

    simple calculator

--- simplecalculator.py
+++ simplecalculator.py
@@ -8,17 +8,24 @@ Operations:
 4.Division
 
 ***************""")
-number1=int(input("please enter a number1:"))
-number2=int(input("please enter a number2:"))
-operation=int(input("please enter operation:"))
 
-if(operation == 1):
+try:
+     number1=int(input("please enter a number1:"))
+     number2=int(input("please enter a number2:"))
+except:
+    print("please enter a integer value")     
+
+operation=(input("please enter operation:"))
+
+
+
+if(operation == "1"):
     print("The sum of the two numbers is",number1+number2)
-elif(operation ==2):
+elif(operation =="2"):
     print("result of subtraction is",number1-number2)
-elif(operation ==3 ):
+elif(operation =="3" ):
     print("product of two numbers is ",number1*number2)
-elif(operation==4):
+elif(operation=="4"):
     print("division of two numbers is ",number1/number2)
 else:
     print("please enter the correct operation")    
