ce71fd2a0b01a7ba174e5a528da1d03faa2c71c7
aysu <aysudeliahmetoglu@gmail.com>
2022-03-03 23:43:32 +0300

    user login system

--- userlogin.py
+++ userlogin.py
@@ -4,11 +4,11 @@ user login
 
 ********** """)
 
-sys_username="\nusername_user"
+sys_username="username_user"
 sys_password=123456
 
-username=input("please enter your username")
-password=int(input("please enter your password"))
+username=input("please enter your username:\n")
+password=int(input("please enter your password:\n"))
 
 if(username==sys_username and password!=sys_password):
     print("password is uncorrect please check your password!")
