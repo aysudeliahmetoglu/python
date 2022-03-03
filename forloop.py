b8afff10e6be79544bd730ce53358061aad09429
aysu <aysudeliahmetoglu@gmail.com>
2022-03-03 23:58:12 +0300

    for loop

---
+++ forloop.py
@@ -0,0 +1,9 @@
+#in operator Checks if an element is in another list, tuple, string
+print(5 in [1,2,3,4]) #false (searching in list)
+print("he"in "hello world") #true (searching in string)
+print(4 in (1,2,3,4,5)) #true (searching in tupple)
+
+#for loop -For loop loops through lists, tuples, strings-
+list=[1,2,3,4,5]
+for element in list:
+    print(element)
