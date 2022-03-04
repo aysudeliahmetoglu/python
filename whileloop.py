77baf1cab475ae2886b86b885d39d606f101195f
aysu <aysudeliahmetoglu@gmail.com>
2022-03-04 23:08:03 +0300

    while loop

---
+++ whileloop.py
@@ -0,0 +1,19 @@
+i=0
+while(i<10):
+    print("value of i:",i)
+    i+=1
+
+while(i<20):
+    print("value of i:",i)
+    i+=2 
+
+j=0
+while(j<5):
+    print("I am learning Python.")
+    j+=1   
+
+list1=[1,2,3,4,5]
+index=0
+while(index<len(list1)):
+    print("index: ",index, "element of list:",list1[index])
+    index +=1
