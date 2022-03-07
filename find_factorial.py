c2cb3cf9c32f053c5c0bc045f392486ee23a5be9
aysu <aysudeliahmetoglu@gmail.com>
2022-03-08 00:07:40 +0300

    Factorial Finder Program

--- find_factorial.py
+++ find_factorial.py
@@ -1,1 +1,20 @@
 #Find Factorial
+print("""***************
+
+Factorial Finder Program
+
+Press 'q' to exit.
+
+*******************
+""")
+while True:
+    number = input("Please enter number:")
+    if(number == "q"):
+        print("The program is terminated.")
+        break
+    else:
+        number=int(number)
+        Factorial=1
+        for i in range(2,number+1):
+            Factorial *=i
+        print("Factorial : ",Factorial)
