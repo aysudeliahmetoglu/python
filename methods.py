4f1490f944350f9cd7bacc7a4015eab25f53bc22
aysu <aysudeliahmetoglu@gmail.com>
2022-03-08 13:23:04 +0300

    methods

---
+++ methods.py
@@ -0,0 +1,18 @@
+#methods
+
+list1=[1,2,3,4,5,6,7,8]
+
+list1.append(9)   #adding element
+print(list1)
+list1.insert(1, "python") # insertion
+print(list1)
+list1.pop()        #subtraction
+print(list1)
+
+list2=["Python","JAVA","C","C#","SQL"]
+list2.append("C ++")
+print(list2)
+list2.insert(2,"GO")
+print(list2)
+list2.pop()
+print(list2)
