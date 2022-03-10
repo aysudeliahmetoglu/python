63223cef88f085698ec66bbccb8117aef6b06d6a
aysu <aysudeliahmetoglu@gmail.com>
2022-03-11 01:02:21 +0300

    return in functions

--- return_in_functions.py
+++ return_in_functions.py
@@ -1,13 +1,34 @@
 #return -used to return value-
 
 def addition(a,b,c,):
-    print("sum= ",a+b+c)
+    print("sum = ",a+b+c)
 
 def multiplyByTwo(a):
-    print("result= ",a*2)
+    print("result = ",a*2)
 
 
-sum=addition(a, b, c) 
-multiplyByTwo(sum)   
+# sum=addition(a, b, c)  -not valued-
+# multiplyByTwo(sum)   
 
-#...
+def addition(a,b,c):
+    return a+b+c
+def multiplyByTwo(a):
+    return a*2
+
+sum = addition(3,4,5)
+print(multiplyByTwo(sum))     
+
+
+def multiplyByThree(a):
+    print("1st function worked")
+    return a*3
+def addUptoFive(a):
+    print("2nd function worked")
+    return a+5
+def divideByFour(a):
+    print("3rd function worked")
+    return a/5    
+
+print(multiplyByThree(addUptoFive(divideByFour(5))))
+
+#Functions that do not return any value where they work in the function are called void
