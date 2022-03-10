9d17c6d8a86c62b8d86b0b2bb3ec38f3beeb1649
aysu <aysudeliahmetoglu@gmail.com>
2022-03-10 20:24:40 +0300

    return in functions

---
+++ return_in_functions.py
@@ -0,0 +1,13 @@
+#return -used to return value-
+
+def addition(a,b,c,):
+    print("sum= ",a+b+c)
+
+def multiplyByTwo(a):
+    print("result= ",a*2)
+
+
+sum=addition(a, b, c) 
+multiplyByTwo(sum)   
+
+#...
