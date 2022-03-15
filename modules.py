b62429731090ec3ecb69a340069f3fbdaaf8b68b
aysu <aysudeliahmetoglu@gmail.com>
2022-03-15 14:44:36 +0300

    using module

--- modules.py
+++ modules.py
@@ -22,3 +22,21 @@ from math import factorial,ceil  #to include only the functions we want
                                   #If functions other than these are used, it will give an error.
 
 print(floor(5.1)) #it will give an error.
+
+print(maths.factorial(5))
+
+ #The last function we defined works. If we added the last module, 
+ # #the module works. If we defined the last function, the function works.
+from math import*
+def factorial(number):
+    print("my function is working.")
+    factorial=1
+    if (number==0 or number==1):
+         return 1
+    else:
+        while (number >=1):
+            factorial*=number    
+            number -=1
+        return factorial
+print(factorial(5))
+
