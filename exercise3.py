cfed4f1ca074ea795f92a9cd6c627497f97af7db
aysu <aysudeliahmetoglu@gmail.com>
2022-03-15 18:01:46 +0300

    exercise 3

---
+++ exercise3.py
@@ -0,0 +1,11 @@
+#Write a program that finds the positive or negative shape of the given integer.
+
+number=int(input("please enter number:"))
+
+if(number<0):
+    print("number is negative")
+
+elif(number>0):
+    print("number is positive")
+else:
+    print("number is 0")        
