bbd1ec437f77f1ca2141d5c510b38161d56d1fdf
aysu <aysudeliahmetoglu@gmail.com>
2022-03-17 00:28:04 +0300

    exercise 7

---
+++ exercise7.py
@@ -0,0 +1,11 @@
+#Write a program that gives the sum and average of five numbers
+
+number1=int(input("please enter number1: "))
+number2=int(input("please enter number2: "))
+number3=int(input("please enter number3: "))
+number4=int(input("please enter number4: "))
+number5=int(input("please enter number5: "))
+sum=number1+number2+number3+number4+number5
+average=sum/5
+print("sum is ",sum)
+print("average is ",average)
