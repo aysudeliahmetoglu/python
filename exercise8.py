7183067ad131c9c0cd0d591dd194038b4e42c88b
aysu <aysudeliahmetoglu@gmail.com>
2022-03-17 23:09:02 +0300

    exercise 8

---
+++ exercise8.py
@@ -0,0 +1,5 @@
+#Write a program that converts Fahrenheit degrees entered on the keyboard to Cantigrades
+
+fahrenheit_degree=int(input("please enter degrees in fahrenheit: "))
+cantigrad=(fahrenheit_degree-32)*5/9
+print(cantigrad)
