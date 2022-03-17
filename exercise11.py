dc1e5139cbace8400a769e479d68984cfcdb0f24
aysu <aysudeliahmetoglu@gmail.com>
2022-03-18 02:20:20 +0300

    exercise 11

---
+++ exercise11.py
@@ -0,0 +1,5 @@
+#Write a program that calculates the area of ​​a triangle  whose height and base length are entered from the keyboard.
+triangle_height=int(input("please enter height of triangle = "))
+triangle_length=int(input("please enter length of triangle = "))
+triangle_area=(triangle_height*triangle_length)/2
+print("area of triangle is ",triangle_area)
