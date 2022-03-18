e50a22d9c2a7b94a782c71553eaa710648f42274
aysu <aysudeliahmetoglu@gmail.com>
2022-03-18 16:59:23 +0300

    exercise 12

---
+++ exercise12.py
@@ -0,0 +1,7 @@
+#The voltage (V) between the poles of a conductor is equal to the product of the current (I) in the ampere type passing through the conductor, multiplied by the resistance (R) on the conductor. It is represented by the formula V=I*R. 
+# Using the formula, write a program that calculates the voltage between the poles of the conductor according to the current and resistance values ​​entered by the user.
+
+current=int(input("Please enter current value: "))
+ampere=int(input("pelase enter ampere value: "))
+voltage=current*ampere
+print("value of voltage is ",voltage)
