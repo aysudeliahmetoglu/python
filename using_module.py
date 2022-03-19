b92104f6f8db8654deadec8ef5c3c59c16fec0c9
aysu <aysudeliahmetoglu@gmail.com>
2022-03-19 23:19:37 +0300

    using our own module

---
+++ using_module.py
@@ -0,0 +1,5 @@
+import write_our_own_module
+
+print(write_our_own_module.addition(5,6,7))
+
+print(write_our_own_module.calculateMean(6,7,9))
--- write_our_own_module.py
+++ write_our_own_module.py
@@ -1,1 +1,23 @@
 #write our own module
+
+programming_languages=["Python","C","Java","C#"]
+def addition(number1,number2,number3):
+    """
+    This function return sum of three number
+
+    """
+    sum=number1+number2+number3
+    
+    return sum
+    
+
+def calculateMean(x1,x2,x3):
+    """
+    This function returns the arithmetic mean of the elements in the list
+
+    """
+    
+    sum=x1+x2+x3
+    return sum/3
+    
+        
