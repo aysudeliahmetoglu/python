09704156ab53a701626f2ecf17dae061b25ed3fa
aysu <aysudeliahmetoglu@gmail.com>
2022-03-20 21:44:51 +0300

    using our own modules

--- using_module.py
+++ using_module.py
@@ -1,5 +1,5 @@
 import write_our_own_module
 
 print(write_our_own_module.addition(5,6,7))
-
-print(write_our_own_module.calculateMean(6,7,9))
+list1=[1,2,3,4]
+print(write_our_own_module.calculateMean(list1))
--- write_our_own_module.py
+++ write_our_own_module.py
@@ -11,13 +11,23 @@ def addition(number1,number2,number3):
     return sum
     
 
-def calculateMean(x1,x2,x3):
+def calculateMean(list1):
+    
     """
     This function returns the arithmetic mean of the elements in the list
 
     """
     
-    sum=x1+x2+x3
-    return sum/3
+    sum=0
+    for i in list1:
+        print(list1[i-1])
+        sum=sum+list1[i-1]
+    return sum/len(list1)
+    
+   
+
+
+    
+
     
         
