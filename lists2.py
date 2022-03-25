c4ac751488e0d2d41a1048f79e462bb4b15add74
aysu <aysudeliahmetoglu@gmail.com>
2022-03-25 23:36:32 +0300

    lists

---
+++ lists2.py
@@ -0,0 +1,17 @@
+import random
+wait_time=random.randint(1,60)
+print(wait_time)
+
+#mutable -list
+#inmutable -tuple cannot be change under any circumstance.Tuple use as a constant list.
+#dictionary - an unordered data structure (unordered and mutable). A dictonary stores key /values pairs.
+temps=[32.0,212.0,0.0,81.6,95,4]
+words=['hello','python']
+print(words)
+car_details=['opel','corsa','black','1.6','10200']
+everything=[temps,words,car_details]
+print(everything)
+
+
+
+
