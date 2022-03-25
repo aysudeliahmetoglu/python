9aa80e95cc5a8a9683b6d6b3c554371029e7415f
aysu <aysudeliahmetoglu@gmail.com>
2022-03-25 23:58:45 +0300

    lists

--- lists2.py
+++ lists2.py
@@ -11,6 +11,39 @@ print(words)
 car_details=['opel','corsa','black','1.6','10200']
 everything=[temps,words,car_details]
 print(everything)
+odds_and_ends=[[1,3,5,7],['a','b','c',],['one','two','three']]  #lists inside of a list
+print(odds_and_ends)
+
+vowels=['a','e','i','o','u']
+word="Milliways"
+for letter in word:
+    if letter in vowels:
+        print(letter)
+
+found=[]
+print(len(found))
+found.append('a')
+found.append('e')
+found.append('i')
+found.append('o')
+
+if 'u' not in found:
+    found.append('u')
+print(found)  
+
+
+vowels=['a','e','i','o','u']
+word=input("Provide a word to search for vowels")
+found=[]
+for letter in word:
+    if letter in vowels:
+        if letter not in found:
+            found.append(letter)
+for vowels in word:
+    print(vowels)            
+
+
+  
 
 
 
