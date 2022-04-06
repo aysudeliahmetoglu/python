78dfc45990e551095f89fce2702c965f228a1d61
aysu <aysudeliahmetoglu@gmail.com>
2022-04-06 17:22:24 +0300

    instagram takipçi listesi

---
+++ instagram.py
@@ -0,0 +1,24 @@
+# Get instance
+import instaloader
+
+# Login or load session
+username = "denemeaccount4"
+password = "pythoncalisma"
+L = instaloader.Instaloader()
+L.login(username, password)
+
+# Obtain profile metadata
+profile = instaloader.Profile.from_username(L.context,"hsdyalova")
+
+# Print list of followees
+
+for followee in profile.get_followers():
+    follow_list = []
+    count = 0
+    follow_list.append(followee.username)
+    file = open("hsdyalovatakipci.txt", "a+")
+    file.write(follow_list[count])
+    file.write("\n")
+    file.close()
+    print(follow_list[count])
+    count = count + 1
