adf94091eb103c16ad10699f52376c132ec420aa
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-04-18 00:41:26 +0300

    instagram folloers count

--- instagram_followers.py
+++ instagram_followers.py
@@ -1,14 +1,11 @@
-import requests
-from bs4 import BeatifulSoup
+import instaloader
 
-username = "hsdturkey"
-url = 'https://www.instagram.com/' + username
-r = requests.get(url)
-soup=BeatifulSoup(r.content)
-followers=soup.find()
-
-start = '"edge_follow":{"count":'
-end = '},"follows_viewer"'
-following= r[r.find(start)+len(start):r.rfind(end)]
-
-print(followers, following)
+instagram = instaloader.Instaloader()
+instagram.load_session_from_file("denemeaccount4")
+accounts=["hsdyalova","hsdturkey","hsdgalata","hsdelazig","hsdgaun","hsdkaraman","hsdmef","hsdankara","hsddpu","hsdbtu","hsdokanuni","hsddenizli"]
+i=0
+while i < len(accounts):
+  
+    target_profile = instaloader.Profile.from_username(instagram.context,accounts[i])
+    print(accounts[i]+ " takipci sayisi:", target_profile.followers)
+    i+=1
