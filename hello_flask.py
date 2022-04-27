824189128fb09424aa1a64a453e4ec596900a8cf
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-04-28 01:51:01 +0300

    flask continue

--- hello_flask.py
+++ hello_flask.py
@@ -1,22 +1,33 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from vsearch import search4letters
 
 app=Flask(__name__)
 
-@app.route("/")   #annotation
-def F()->str:
-    return "Hello World"
+
+
+
 # @app.route("/categories")   #writing route
 # def F2()->str:
 #     return "Hello Categories"
-@app.route("/search4") 
-def do_search()->str:
-    res= str(search4letters("bir phrase gönderiyoruz","aeiöuüı"))
+
+@app.route("/search4",methods=["POST"]) 
+def do_search()->"html":
+    phrase=request.form["phrase"]
+    letters=request.form["letters"]
+    title="Welcome to the search4letters website"
+
+    res= str(search4letters(phrase,letters))
     print(res)
-    return res
+    return render_template("results.html",
+    the_title=title,
+    the_phrase=phrase,
+    the_letters=letters,
+    the_result=res
+    )
+@app.route("/")   #annotation
 @app.route("/entry") 
 def entry_page()->"html":
-    title="Welcome to the search4letters website"
+    title="Welcome to the search4letters website!"
     return render_template("entry.html",the_title=title)    
 
-app.run()
+app.run(debug=True)
